7750043198a65fa8b5b7039426b0310f72a00c4d
Reza Hajianpour <hajianpour.mr@gmail.com>
2024-04-07 02:35:53 +0330

    add: home page revamp using meshki css

--- include/templates/pages/home.h
+++ include/templates/pages/home.h
@@ -18,7 +18,28 @@ static Page page_home = Page {
         PAGE_HOME_TITLE,
         // css
         R"=====(
-            
+            <style>
+                .content {
+                    min-height: 50vh;
+                    height: calc(100vh - 52px - 125px);
+                    margin-top: 52px;
+                }
+                @media (min-width: 544px) {
+                    .content {
+                        height: calc(100vh - 52px - 100px);
+                    }
+                }
+
+                @media (min-width: 910px) {
+                    .content {
+                        height: calc(100vh - 52px - 76px);
+                    }
+                }
+                .footer {
+                    position: fixed;
+                    bottom: 0;
+                }
+            </style>
         )=====",
         // js
         R"=====(
@@ -26,11 +47,85 @@ static Page page_home = Page {
         )=====", 
         // body
         R"=====(
-            <h1>Welcome to the Home Page</h1>
-            <ul>
-                <li><a href="/led">LED Page</a></li>
-                <li><a href="/temperature">Temperature Page</a></li>
-            </ul>
+            <div class="sidenav push">
+                <a href="javascript:void(0);" class="sidenav-close-button" onclick="meshki.closeSidenav()">×</a>
+                <a href="/">خانه</a>
+                <a href="https://github.com/Borderliner/Meshki/issues">Issues</a>
+                <a href="https://github.com/Borderliner/Meshki/pulls">Pull Requests</a>
+                <a href="https://github.com/Borderliner/Meshki/releases">Releases</a>
+                <a href="rtl/">RTL Mode (Eastern)</a>
+                <a href="https://github.com/Borderliner/Meshki/blob/master/CHANGELOG.md">Changelog</a>
+                <a href="https://github.com/Borderliner/Meshki/blob/master/LICENSE">License</a>
+                <a href="https://plisio.net/donate/VXKr4hHx" target="_blank">Donate</a>
+                <a href="https://github.com/Borderliner/Meshki/blob/master/README.md">About Meshki</a>
+                <hr class="hide-on-non-mobile">
+                <div class="hide-on-non-mobile">
+                    <a href="#buttons-doc" onclick="meshki.closeSidenav()">Buttons</a>
+                    <a href="#sidenav-doc" onclick="meshki.closeSidenav()">Sidenav</a>
+                    <a href="#navbar-doc" onclick="meshki.closeSidenav()">Navbar</a>
+                    <a href="#layout-doc" onclick="meshki.closeSidenav()">Layout System</a>
+                    <a href="#form-doc" onclick="meshki.closeSidenav()">Forms</a>
+                    <a href="#typography-doc" onclick="meshki.closeSidenav()">Typography</a>
+                    <a href="#tables-doc" onclick="meshki.closeSidenav()">Tables</a>
+                    <a href="#footer-doc" onclick="meshki.closeSidenav()">Footer</a>
+                </div>
+                <hr>
+                <button class="button blue medium extern-twitter" onclick="location.href='https://twitter.com/Meshki_UI'"><i class="fa-brands fa-x-twitter" aria-hidden="true"></i></button>
+                <button class="button red medium extern-npm" onclick="location.href='https://www.npmjs.com/package/meshki'"><i class="fab fa-npm" aria-hidden="true"></i></button>
+                <button class="button medium extern-github" onclick="location.href='https://github.com/Borderliner/Meshki'"><i class="fab fa-github" aria-hidden="true"></i></button>
+                <a href="javascript:void(0)" disabled="" style="font-size: 1.5rem">Share on:</a>
+                <div class="share-links">
+                    <a class="fb-share" href="https://www.facebook.com/sharer/sharer.php?u=https://borderliner.github.io/Meshki/" target="_blank"><i class="fab fa-facebook-square" aria-hidden="true"></i></a>
+                    <a class="tw-share" href="https://twitter.com/home?status=https://borderliner.github.io/Meshki/" target="_blank"><i class="fa-brands fa-square-x-twitter" aria-hidden="true"></i></a>
+                    <a class="gp-share" href="https://plus.google.com/share?url=https://borderliner.github.io/Meshki/" target="_blank"><i class="fab fa-google-plus-square" aria-hidden="true"></i></a>
+                </div>
+            </div>
+            <div class="container">
+                <div class="nav fixed">
+                    <ul>
+                        <li><a class="logo" href="javascript:void(0);" onclick="meshki.openSidenav()"><i class="fas fa-bars" aria-hidden="true"></i>منوی اصلی</a></li>
+                        <li class="nav-dropdown hide-on-mobile">
+                        <a href="javascript:void(0)" class="nav-dropdown-button">ابزار</a>
+                        <div class="nav-dropdown-content">
+                            <a href="rtl/">یک</a>
+                            <a href="#extra-button-colors">دو</a>
+                        </div>
+                        </li>
+                        <li><a href="https://github.com/Borderliner/Meshki">پیوندها</a></li>
+                        <li class="nav-dropdown hide-on-mobile">
+                            <a href="javascript:void(0)" class="nav-dropdown-button">مدیریت</a>
+                            <div class="nav-dropdown-content">
+                                <a href="#buttons-doc">Buttons</a>
+                                <a href="#sidenav-doc">Sidenav</a>
+                                <a href="#navbar-doc">Navbar</a>
+                                <a href="#layout-doc">Layout System</a>
+                                <a href="#form-doc">Forms</a>
+                                <a href="#typography-doc">Typography</a>
+                                <a href="#tables-doc">Tables</a>
+                                <a href="#footer-doc">Footer</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="content">
+                    <h1>به پنل مدیریتی خوش آمدید!</h1>
+                    <ul>
+                        <li><a href="/led">تنظیمات LED</a></li>
+                        <li><a href="/temperature">نمایشگر دما</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer">
+                    <div class="row">
+                        <div class="six col">
+                            <p>ساخته شده توسط محمدرضا حاجیانپور (<a href="https://github.com/Borderliner">Borderliner</a>)</p>
+                        </div>
+                        <div class="six col text-center">
+                            <p>تمامی حقوق این نرم افزار طبق قانون برای ناشر محفوظ می‌باشد.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )=====",
         // header_template
         "",
