7acf827b81853da1123133b96409374c277fbe86
Reza Hajianpour <hajianpour.mr@gmail.com>
2024-04-07 02:35:41 +0330

    remove: version number on files due to upload fail

--- include/templates/layout.h
+++ include/templates/layout.h
@@ -10,13 +10,15 @@
 #pragma once
 
 const char* GLOBAL_JAVASCRIPT = R"====(
-    <script src="/assets/js/lib/alpinejs@3.13.8.min.js"></script>
+    <script src="/assets/js/lib/alpinejs.min.js"></script>
+    <script src="/assets/js/lib/meshki.min.js"></script>
     <script src="/assets/js/main.js"></script>
 )====";
 
 const char* GLOBAL_CSS = R"====(
-    <link rel="stylesheet" href="/assets/css/lib/vazirmatn@33.003.css">
-    <link rel="stylesheet" href="/assets/css/lib/meshki@2.5.1.min.css">
+    <link rel="stylesheet" href="/assets/css/lib/vazirmatn.css">
+    <link rel="stylesheet" href="/assets/css/lib/meshki.min.css">
+    <link rel="stylesheet" href="/assets/css/lib/meshki.rtl.min.css">
     <link rel="stylesheet" href="/assets/css/style.css">
 )====";
 
